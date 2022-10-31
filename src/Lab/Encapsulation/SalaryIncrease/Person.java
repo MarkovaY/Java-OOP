eb17ddc5ed06e5c3ef0d3f03fe68ffdfdcd8dc6c
YanaMarkova <yana_markova@yahoo.com>
2022-10-31 16:36:13 +0100

    Initial commit

---
+++ src/Lab/Encapsulation/SalaryIncrease/Person.java
@@ -0,0 +1,42 @@
+package Lab.Encapsulation.SalaryIncrease;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double bonus) {
+        double bonusReduced = age < 30 ? bonus / 200 : bonus / 100;
+
+        this.setSalary(salary + (salary * bonusReduced));
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %.1f leva", this.getFirstName(), this.getLastName(), this.getSalary());
+    }
+}

cce86d28b9fa1f545d990d3b95e037d8a8f1455a
YanaMarkova <yana_markova@yahoo.com>
2022-11-02 15:35:52 +0100

    Initial commit

---
+++ src/Lab/Encapsulation/FirstAndReserveTeam/Person.java
@@ -0,0 +1,72 @@
+package Lab.Encapsulation.FirstAndReserveTeam;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+        setSalary(salary);
+    }
+
+    public void increaseSalary(double bonus) {
+        double bonusReduced = age < 30 ? bonus / 200 : bonus / 100;
+
+        this.setSalary(salary + (salary * bonusReduced));
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 460.0) {
+            throw new IllegalStateException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        ensureProperNameLength(firstName, "First");
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        ensureProperNameLength(lastName, "Last");
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalStateException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    private void ensureProperNameLength(String name, String messagePrefix) {
+        if (name.length() < 3) {
+            throw new IllegalStateException(messagePrefix + " name cannot be less than 3 symbols");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %.1f leva", this.getFirstName(), this.getLastName(), this.getSalary());
+    }
+}

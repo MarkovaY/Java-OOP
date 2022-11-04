ad2c7c022bb16980c00c1ddeb1eb904abb3223b0
YanaMarkova <yana_markova@yahoo.com>
2022-11-04 16:36:50 +0100

    Initial commit

--- src/Lab/Inheritance/MultipleInheritance/Dog.java
+++ src/Lab/Inheritance/MultipleInheritance/Dog.java
@@ -1,7 +1,5 @@
 package Lab.Inheritance.MultipleInheritance;
 
-import Lab.Inheritance.MultipleInheritance.Animal;
-
 public class Dog extends Animal {
     public void bark (){
         System.out.println("barking...");

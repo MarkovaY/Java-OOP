649c0a223337efc34b18a04f1ce8ff03decadc1f
YanaMarkova <yana_markova@yahoo.com>
2022-11-08 16:06:58 +0100

    Initial commit

--- src/Lab/Inheritance/IncreasingHierarchy/Main.java
+++ src/Lab/Inheritance/IncreasingHierarchy/Main.java
@@ -3,7 +3,7 @@ package Lab.Inheritance.IncreasingHierarchy;
 public class Main {
     public static void main(String[] args) {
         // Training in creating child classes of the child classes
-        // Puppy extends Dog which extends Animal
+        // Puppy extends Dog which extends Exercise.Inheritance.Zoo.Animal
         // Puppy has to use all the methods from the three classes
         Puppy puppy = new Puppy();
         puppy.eat();

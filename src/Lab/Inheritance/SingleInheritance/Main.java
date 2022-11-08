be29b60798aeb1cec8e43391a7c2d46bd552f3bc
YanaMarkova <yana_markova@yahoo.com>
2022-11-08 16:07:14 +0100

    Initial commit

--- src/Lab/Inheritance/SingleInheritance/Main.java
+++ src/Lab/Inheritance/SingleInheritance/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Learning the basics of inheritance
-        // Creating class Dog which extends class Animal, with two simple methods for eating and barking
+        // Creating class Dog which extends class Exercise.Inheritance.Zoo.Animal, with two simple methods for eating and barking
         Dog dog = new Dog();
         dog.eat();
         dog.bark();

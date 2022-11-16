25a54a763323920a9e269d49e6d77fc63988f6e5
YanaMarkova <yana_markova@yahoo.com>
2022-11-16 15:07:48 +0100

    Initial commit

--- src/Exercise/Polymorphism/MathOperation/Main.java
+++ src/Exercise/Polymorphism/MathOperation/Main.java
@@ -3,10 +3,9 @@ package Exercise.Polymorphism.MathOperation;
 public class Main {
     public static void main(String[] args) {
 
-        MathOperation math = new MathOperation();
-        System.out.println(math.add(2, 2));
-        System.out.println(math.add(3, 3, 3));
-        System.out.println(math.add(4, 4, 4, 4));
+        System.out.println(MathOperation.add(2, 2));
+        System.out.println(MathOperation.add(3, 3, 3));
+        System.out.println(MathOperation.add(4, 4, 4, 4));
 
     }
 }
--- src/Exercise/Polymorphism/MathOperation/MathOperation.java
+++ src/Exercise/Polymorphism/MathOperation/MathOperation.java
@@ -2,15 +2,15 @@ package Exercise.Polymorphism.MathOperation;
 
 public class MathOperation {
 
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 
-    public int add(int a, int b, int c) {
+    public static int add(int a, int b, int c) {
         return a + b + c;
     }
 
-    public int add(int a, int b, int c, int d) {
+    public static int add(int a, int b, int c, int d) {
         return a + b + c + d;
     }
 }

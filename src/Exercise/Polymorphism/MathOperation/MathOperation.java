471a0f7304ba4eb354cb03dd3e415853438d5b9d
YanaMarkova <yana_markova@yahoo.com>
2022-11-16 15:11:32 +0100

    Initial commit

--- src/Exercise/Polymorphism/MathOperation/MathOperation.java
+++ src/Exercise/Polymorphism/MathOperation/MathOperation.java
@@ -7,10 +7,10 @@ public class MathOperation {
     }
 
     public static int add(int a, int b, int c) {
-        return a + b + c;
+        return add(a, add(b, c));
     }
 
     public static int add(int a, int b, int c, int d) {
-        return a + b + c + d;
+        return add(a, add(b, c, d));
     }
 }

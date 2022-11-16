0ee0698f57d7a9b32b29805c8d32a7530c2e78c1
YanaMarkova <yana_markova@yahoo.com>
2022-11-16 16:54:26 +0100

    Initial commit

---
+++ src/Exercise/Polymorphism/Animals/Cat.java
@@ -0,0 +1,13 @@
+package Exercise.Polymorphism.Animals;
+
+public class Cat extends Animal {
+
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("%s%nMEEOW", super.explainSelf());
+    }
+}

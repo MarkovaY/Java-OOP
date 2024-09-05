549f6becd7cf256eeb6e79f93dfddc94ff5827d7
YanaMarkova <yana_markova@yahoo.com>
2024-09-05 17:14:47 +0200

    Initial commit

--- src/Lab/WorkingWithAbstraction/StudentSystem/Main.java
+++ src/Lab/WorkingWithAbstraction/StudentSystem/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         Lab.WorkingWithAbstraction.StudentSystem.StudentSystem studentSystem = new Lab.WorkingWithAbstraction.StudentSystem.StudentSystem();

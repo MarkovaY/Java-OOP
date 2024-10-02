d1346af90e60fb8733fd240392513c937317b72b
YanaMarkova <yana_markova@yahoo.com>
2024-10-02 17:20:49 +0200

    Initial commit

--- src/Lab/ExceptionsAndErrorHandling/NumberInRange/Main.java
+++ src/Lab/ExceptionsAndErrorHandling/NumberInRange/Main.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-//        Write a program to enter an integer in a certain range.
-//        Until the number is not valid, enter a new number from the console.
-//        When the number is valid - end the program.
+//  Write a program to enter an integer in a certain range.
+//  Until the number is not valid, enter a new number from the console.
+//  When the number is valid - end the program.
 
         Scanner scanner = new Scanner(System.in);
 
-//          Read a range - two numbers, separated by a space.
+//  Read a range - two numbers, separated by a space.
         int[] range = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
-//        	Print the range in the following format: "Range: [{startIndex}...{endIndex}]".
+//  Print the range in the following format: "Range: [{startIndex}...{endIndex}]".
         System.out.println("Range: ["+ range[0] + "..." + range[1] + "]");
 
         boolean isValidInput = false;
 
-//        	On the next line, read the String.
+//  On the next line, read the String.
         while(!isValidInput) {
 
             String nextInput = scanner.nextLine();
@@ -33,10 +33,10 @@ public class Main {
             } catch(NumberFormatException ignored){
             }
 
-//          When an invalid number is entered, String or the number is out of range, print "Invalid number: {num}".
+//  When an invalid number is entered, String or the number is out of range, print "Invalid number: {num}".
             String output = "Invalid number: " + nextInput;
 
-//        	When the entered number is valid, print "Valid number: {num}".
+//  When the entered number is valid, print "Valid number: {num}".
             if(number.isPresent() && isInRange(range, number.get())){
                 output = "Valid number: " + number.get();
                 isValidInput = true;
@@ -46,6 +46,7 @@ public class Main {
         }
     }
 
+//  Check if the given number is in the range
     private static boolean isInRange(int[] range, int number) {
         return number >= range[0] && number <= range[1];
     }
